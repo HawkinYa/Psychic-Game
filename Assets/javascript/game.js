e2ea8e004621c49df7b3f7c537f5ea0ceb6577b3
Hunter Storm Hawk <hunterhawk1021@gmail.com>
2018-11-20 19:38:58 -0700

    just rolling to get this entered in

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -13,38 +13,41 @@ var guessesLeft = 9;
 // function that executes when user pushes a button
 document.onkeyup = function (event) {
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerLetter[Math.floor(Math.random() * computerLetter.length)];
-    console.log(computerGuess);
-
     // Determines which key was pressed.
     var userGuess = event.key;
     console.log(userGuess);
 
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = computerLetter[Math.floor(Math.random() * computerLetter.length)];
+    console.log(computerGuess);
+
     // Was it a win??
     if (userGuess == computerGuess) {
         wins++;
-    }
+    };
+    
+    // Or a loss..?
+    if (userGuess != computerGuess); {
+        losses++;
+    };
 
     // If you guessed wrong, which was it?
     function addTo() {
         wrongGuesses.push(("userGuess").value);
         document.getElementById("guesses").innerHTML = "Guesses Left:" + userGuess;
         console.log(wrongGuesses);
-    }
+    };
 
     // And how may guesses do you have left?
-    // else if (userGuess != computerGuess) {
-    //     for (userGuess = 9; userGuess = 0; i--) {
-    //         guess--;
-    //     }
+    if (userGuess != computerGuess) {
+        guessesLeft--;
+    };
 
-    // }
-
-    // And how may guesses do you have left?
+    
 
     document.getElementById("wins").innerHTML = "Wins:" + wins;
     document.getElementById("losses").innerHTML = "Losses:" + losses;
     document.getElementById("guesses").innerHTML = "Guesses Left:" + guessesLeft;
+    document.getElementById("alreadyGuessed").innerHTML = "Wrong Guesses:" + userGuess;
 
 };
