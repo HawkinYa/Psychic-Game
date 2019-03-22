911d557d7b7c3a383dc615c904ac5f83724ce885
Hunter Storm Hawk <hunterhawk1021@gmail.com>
2019-03-21 17:43:17 -0700

    finally an A other than the prework...

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,53 +1,64 @@
+var game = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-
-
-// array that lists all the computer choices
-var computerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// starting variables for how many wins, losses, wrong letters and guesses there are
 var wins = 0;
 var losses = 0;
-var wrongGuesses = [""]
-var guessesLeft = 9;
+var guessesLeft = 10;
+var guesses = " ";
+var userGuess = [];
+var lettersGuessed = []
+var games = []
+var randomItem = game[Math.floor(Math.random() * game.length)];
 
-// function that executes when user pushes a button
+
+//Chooses random letter
+
+function reset() {
+    randomItem = game[Math.floor(Math.random() * game.length)];
+    console.log(randomItem);
+    document.getElementById("guess-so-far").innerHTML = ""
+}
+//pushes random letter to guesses so far
 document.onkeyup = function (event) {
+    userGuess = event.key;
+    lettersGuessed.push(userGuess);
+    document.getElementById("guess-so-far").innerHTML = lettersGuessed; console.log("computer:", randomItem)
 
-    // Determines which key was pressed.
-    var userGuess = event.key;
-    console.log(userGuess);
+    //win coding
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerLetter[Math.floor(Math.random() * computerLetter.length)];
-    console.log(computerGuess);
+    if (userGuess == randomItem) {
+        wins++
+        alert("You win!!!!");
+        lettersGuessed = [];
+        guessesLeft = 10;
+        document.getElementById("wins").innerHTML = wins;
+        randomItem = "";
 
-    // Was it a win??
-    if (userGuess == computerGuess) {
-        wins++;
-    };
-    
-    // Or a loss..?
-    if (userGuess != computerGuess); {
-        losses++;
-    };
-
-    // If you guessed wrong, which was it?
-    function addTo() {
-        wrongGuesses.push(("userGuess").value);
-        document.getElementById("guesses").innerHTML = "Guesses Left:" + userGuess;
-        console.log(wrongGuesses);
-    };
-
-    // And how may guesses do you have left?
-    if (userGuess != computerGuess) {
+        reset();
+    }
+    else {
+        //loss coding
         guessesLeft--;
-    };
+        document.getElementById("guess-left").innerHTML = guessesLeft;
+        if (guessesLeft === 0) {
+            alert("You lose!");
+        }
+        if (guessesLeft < 1) {
+            losses++;
+            lettersGuessed = [];
+            guessesLeft = 10;
+            document.getElementById("losses").innerHTML = losses;
+            reset();
+        }
 
-    
 
-    document.getElementById("wins").innerHTML = "Wins:" + wins;
-    document.getElementById("losses").innerHTML = "Losses:" + losses;
-    document.getElementById("guesses").innerHTML = "Guesses Left:" + guessesLeft;
-    document.getElementById("alreadyGuessed").innerHTML = "Wrong Guesses:" + userGuess;
+    }
 
+}
+
+
+
+
+
+function restart() {
+    location.reload();
 };
